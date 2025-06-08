ef55b066bffd0e3ff697ac874676f20fae175435
moo-ongsa <ongsa.sungkanit@gmail.com>
2025-06-08 22:24:00 +0700

    remove hello

--- src/app/api/hello/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request) {
-   throw new Error("Custom error message");
-  // return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-}
